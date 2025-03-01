f39fcddb499f3adb886ec989c01aa0036005449d
Deep Gupta <deep.22gcebai036@galgotiacoleege.edu>
2025-03-01 14:43:12 +0530

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Navbar } from "./components/Navbar"
 import { Hero } from "./components/Hero"
 import { About } from "./components/About"
 import { Technologies } from "./components/Technologies"
+import { Analytics } from "@vercel/analytics/react"
 
 import { Projects } from "./components/Projects"
 import { Contact } from "./components/Contact"
@@ -19,6 +20,7 @@ function App() {
       <Technologies/>
       <Projects/>
       <Contact/>
+      <Analytics/>
     </div>
   </div>
 }
