70c20676f234ecf6cd00ab79d7c9302aa58f5d6a
Deep Gupta <deep.22gcebai036@galgotiacoleege.edu>
2025-03-01 14:30:40 +0530

    technologies update

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,9 +1,9 @@
 import{ RiReactjsLine } from "react-icons/ri"
 import{ TbBrandNextjs } from "react-icons/tb"
 import{ SiMongodb } from "react-icons/si"
-import{ DiRedis } from "react-icons/di"
 import{ FaNodeJs } from "react-icons/fa"
-import{ BiLogoPostgresql } from "react-icons/bi"
+import { TbBrandCpp } from "react-icons/tb";
+import { FaPython } from "react-icons/fa";
 import { motion } from "framer-motion";
 
 const iconVariants = (duration) => ({
@@ -54,13 +54,6 @@ export const Technologies = () => {
               <SiMongodb className="text-7xl text-green-500" />
             </motion.div>
             <motion.div
-            variants={iconVariants(2)}
-            initial= "initial"
-            animate= "animate"  
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-              <DiRedis className="text-7xl text-red-700" />
-            </motion.div>
-            <motion.div
             variants={iconVariants(6)}
             initial= "initial"
             animate= "animate"  
@@ -72,7 +65,14 @@ export const Technologies = () => {
             initial= "initial"
             animate= "animate"  
             className="rounded-2xl border-4 border-neutral-800 p-4">
-              <BiLogoPostgresql className="text-7xl text-sky-700" />
+              <TbBrandCpp className="text-7xl text-sky-700" />
+            </motion.div>
+            <motion.div
+            variants={iconVariants(4)}
+            initial= "initial"
+            animate= "animate"  
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+              <FaPython className="text-7xl text-[#FFE873]" />
             </motion.div>
 
         </motion.div>
